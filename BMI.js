7df84c1d1fb400f33f585e3c3535d789e96c7e46
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-03-31 10:41:46 -0700

    Updtaed BMI Calculator

--- BMI.js
+++ BMI.js
@@ -2,8 +2,8 @@ import promptSync from 'prompt-sync';
 import chalk from 'chalk';
 const prompt = promptSync();
 function BmiCal() {
-    let weight = parseInt(prompt(chalk.cyan("Enter you weight in kg : ")));
-    let height = parseInt(prompt(chalk.cyan("Enter you height in meters : ")));
+    let weight = Number(prompt(chalk.cyan("Enter you weight in kg : ")));
+    let height = Number(prompt(chalk.cyan("Enter you height in meters : ")));
     console.log(chalk.yellow.bold("BMI (Body Mass Index) is :"), chalk.magentaBright(weight / (height * height)));
 }
 BmiCal();
--- BMI.ts
+++ BMI.ts
@@ -3,8 +3,8 @@ import chalk from 'chalk';
 const prompt = promptSync();
 
 function BmiCal(){
-    let weight = parseInt(prompt(chalk.cyan("Enter you weight in kg : ")));
-    let height = parseInt(prompt(chalk.cyan("Enter you height in meters : ")));
+    let weight = Number(prompt(chalk.cyan("Enter you weight in kg : ")));
+    let height = Number(prompt(chalk.cyan("Enter you height in meters : ")));
 console.log(chalk.yellow.bold("BMI (Body Mass Index) is :"),chalk.magentaBright( weight / (height * height)));
 }
 BmiCal();
